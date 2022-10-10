5a4d4a1ba51cc280b29ebb547d73101b3cf23cf6
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-10-09 22:23:56 -0300

    implementações/ Limpeza
    Colocado o Scroll
    e colocado mensagens de sucesso!
    
    Limpeza geral de arquivos e cod sem uso

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
-//import QrCode from "./Pages/home/index";
-
-import Teste from "./Pages/home/teste";
+import QrCode from "./Pages/home/index";
 
 //Pop-up de Alerta
-//import { Slide, ToastContainer } from "react-toastify";
-//import "react-toastify/dist/ReactToastify.css";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
-      {/* <ToastContainer autoClose={3000} transition={Slide} /> */}
-      <Teste />
+      <ToastContainer autoClose={3000} transition={Slide} />
+      <QrCode />
     </>
   );
 }
--- src/Pages/home/index.js
+++ src/Pages/home/index.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-//import { toast } from "react-toastify";
-
+import { useState } from "react";
+import { toast } from "react-toastify";
+import Logo from "../../assets/img/QRCode.gif";
 //Listagem das medidas disponível
 const listMedidas = [
   { id: 1, name: "150x150" },
@@ -24,76 +24,118 @@ const listMedidas = [
 ];
 
 function QrCode() {
-  const [texto, setTexto] = useState("");
   const [link, setLink] = useState("");
-  const [tamanho, setTamanho] = useState("150");
+  const [tamanho, setTamanho] = useState("");
   const [bgColor, setBgColor] = useState("ffffff");
   const [qrCode, setQrCode] = useState("");
-  const rota = `http://api.qrserver.com/v1/create-qr-code/?data=${link}!&size=${tamanho}x${tamanho}&bgcolor=${bgColor}`;
+  const rota = `http://api.qrserver.com/v1/create-qr-code/?data=${link}!&size=${tamanho}&bgcolor=${bgColor}`;
 
   function handleClick() {
     setQrCode(rota);
-    //toast.success("QR-Code Gerado!");
+    toast.success("QR-Code Gerado!");
     setLink("");
+    setTamanho("");
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>QR Code Generator</h1>
-        <div className="input-box">
-          <div className="geracao">
-            <input
-              type="text"
-              value={link}
-              onChange={(e) => {
-                setLink(e.target.value);
-              }}
-              placeholder="Digite o texto"
-            />
-            <button className="button" onClick={handleClick}>
+    <>
+      <h1 className="visually-hidden">Heroes examples</h1>
+
+      <div className="px-4 py-5 my-5 text-center">
+        <img
+          className="d-block mx-auto mb-4"
+          src={Logo}
+          alt=""
+          width="250"
+          height="250"
+        />
+
+        <h1 className="display-5 fw-bold">QR Code Generator</h1>
+        <div className="col-lg-6 mx-auto">
+          <input
+            className="form-control mb-3"
+            type="text"
+            value={link}
+            onChange={(e) => {
+              setLink(e.target.value);
+            }}
+            placeholder="Insira o site ou texto aqui"
+          />
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <div className="container text-center">
+              <div className="row">
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center col">
+                  <h5>Cor de fundo:</h5>
+                  <input
+                    type="color"
+                    className="form-control form-control-color form-control-sm mb-5 tamanhoCor"
+                    onChange={(e) => {
+                      setBgColor(e.target.value.substring(1));
+                    }}
+                  />
+                </div>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center col">
+                  <h5>Dimensão:</h5>
+                  <select
+                    defaultValue={tamanho}
+                    onChange={(e) => setTamanho(e.target.value)}
+                    className="form-select form-select-sm mb-5"
+                    placeholder="Selecione as medidas"
+                  >
+                    <option>Selecione as medidas</option>
+                    {listMedidas.map((item) => (
+                      <option value={item.nome} key={item.id}>
+                        {item.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="#qrcode">
+            <button
+              type="button"
+              className="btn btn-primary px-4 gap-3 col mb-4"
+              onClick={handleClick}
+            >
               Gerar
             </button>
-          </div>
-          <div className="extra">
-            <h5>Cor de fundo:</h5>
-            <input
-              type="color"
-              onChange={(e) => {
-                setBgColor(e.target.value.substring(1));
-              }}
-            />
-            <h5>Dimensão:</h5>
-            <select
-              defaultValue={tamanho}
-              onChange={(e) => setTamanho(e.target.value)}
-              className="form-select"
-              placeholder="Selecione as medidas"
-            >
-              <option selected>Selecione as medidas</option>
-              {listMedidas.map((item, index) => (
-                <option defaultValue={item.id} key={index}>
-                  {item.name}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="output-box">
-          <img src={qrCode} alt="" className="img-fluid" />
-          <a
-            href={qrCode}
-            download="QRCode"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button type="button" className="button">
-              Download
-            </button>
           </a>
+          <p className="lead mb-4">
+            1: Coloque um link ou texto.
+            <br />
+            2: Escolha uma cor e dimensão.
+            <br />
+            3: clique em "GERAR"
+            <br />
+            (QR Code será criado automaticamente)
+          </p>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-3">
+            <img
+              src={qrCode}
+              alt=""
+              className="img-fluid d-block mx-auto mb-1"
+              id="qrcode"
+            />
+          </div>
+          {qrCode && (
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <a
+                href={qrCode}
+                download
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button type="button" className="btn btn-success">
+                  Download
+                </button>
+              </a>
+            </div>
+          )}
         </div>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 export default QrCode;
